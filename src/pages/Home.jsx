32d35a0b66c09189d1d71abb16c206b4a3260913
moeid-526 <moeidabdulhayee@gmail.com>
2026-05-31 22:24:24 +0500

    site icon added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -241,7 +241,7 @@ export default function Home() {
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
-                <i className="fas fa-download"></i> Download CV
+                <i className="fas fa-download"></i> View / Download CV
               </motion.a>
             </motion.div>
 
